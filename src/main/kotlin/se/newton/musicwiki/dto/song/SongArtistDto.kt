5803c683aa098e189db23749bb891419d1a23bc1
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-02-14 17:09:01 +0100

    add type to song artist dto

--- src/main/kotlin/se/newton/musicwiki/dto/song/SongArtistDto.kt
+++ src/main/kotlin/se/newton/musicwiki/dto/song/SongArtistDto.kt
@@ -1,10 +1,12 @@
 package se.newton.musicwiki.dto.song
 
+import se.newton.musicwiki.persistence.enums.ArtistType
 import se.newton.musicwiki.persistence.models.Artist
 
 data class SongArtistDto(
   val id: Long?,
   val name: String?,
+  val type: ArtistType?,
   val longitude: Double?,
   val latitude: Double?,
 )
@@ -13,6 +15,7 @@ fun mapSongArtistToDto(artist: Artist): SongArtistDto {
   return SongArtistDto(
     id = artist.id,
     name = artist.name,
+    type = artist.artistType,
     longitude = artist.longitude,
     latitude = artist.latitude,
   )
